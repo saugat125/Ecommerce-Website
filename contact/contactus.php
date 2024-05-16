a0b18bc435250c9f3c8506c74465cf519d520336
abhishekksarma <abhishekksarma@gmail.com>
2024-05-16 22:45:35 +0545

    contact us updated

--- contact/contactus.php
+++ contact/contactus.php
@@ -12,6 +12,12 @@
 </html>
 <?php include ('../header/header.php') ?>
 
+<section class="contact-us">
+    <div class="contact-heading">
+        <h2>Contact Us</h2>
+    </div>
+</section>
+
     <div class="container">
         <div class="form-container">
             <h2>Contact Us</h2>
@@ -58,4 +64,50 @@
         </div>
     </div>
 
-    <script src="script.js"></script>
+    <footer>
+    <div class="footer-container">
+        <div class="footer-left">
+            <ul>
+                <h3>Overview</h3>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Privacy policy</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">FAQs</a></li>
+            </ul>
+        </div>
+        <div class="footer-middle">
+            <ul>
+                <h3>Available Shops</h3>
+                <li>Haricekts Farm</li>
+                <li>Dyreborn Holland</li>
+                <li>Vitrion</li>
+                <li>Barne Pettinger</li>
+            </ul>
+        </div>
+        <div class="footer-right">
+            <ul>
+                <h3>Shop by Category</h3>
+                <li>Vegetables</li>
+                <li>Fruits</li>
+                <li>Meat & Fish</li>
+                <li>Dairy and Bakery</li>
+            </ul>
+        </div>
+        <div class="footer-contact">
+            <h3>Contact Us</h3>
+            <p>+977-9857011098</p>
+            <p>info@c-fresh.com</p>
+            <p>Clostkhodesfax, UK</p>
+            <h3>Social</h3>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <p>&copy; 2024 - C-Fresh. All Rights Reserved.</p>
+    </div>
+</footer>
