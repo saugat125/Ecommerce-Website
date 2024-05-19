e5cbe0ec89f36a509c695d47a06e3a99e5d93bd6
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2024-05-20 00:10:16 +0545

    shop profile php in trader dashboard

--- trader info/traderinfo.php
+++ trader info/traderinfo.php
@@ -12,11 +12,22 @@ session_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+    <?php
+    $trader_id = $_SESSION['user_id'];
+
+
+    $shop_query = "SELECT * FROM SHOP WHERE TRADER_ID = '$trader_id'";
+
+    $shop_stmt = oci_parse($conn, $shop_query);
+    oci_execute($shop_stmt);
+
+    while ($row = oci_fetch_assoc($shop_stmt)) {
+        ?>
     <header>
         <nav>
             <div class="nav-icons">
-                <span class="icons"><i class="fa fa-bars" aria-hidden="true">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp SHOP</i></span>
-                <span class="user"><i class="fa fa-user-circle" aria-hidden="true"> Harefields fam</i></span>
+                <span class="icons"><i class="fa fa-bars" aria-hidden="true">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &nbsp </i>SHOP</span>
+                <span class="user"><i class="fa fa-user-circle" aria-hidden="true"></i><?php echo $row['SHOP_NAME']; ?></span>
             </div>
         </nav>
     </header>
@@ -30,17 +41,7 @@ session_start();
 
     <div class="shop">
 
-    <?php
-        $trader_id = $_SESSION['user_id'];
-
-
-        $shop_query = "SELECT * FROM SHOP WHERE TRADER_ID = '$trader_id'";
-
-        $shop_stmt = oci_parse($conn, $shop_query);
-        oci_execute($shop_stmt);
-
-        while ($row = oci_fetch_assoc($shop_stmt)){
-    ?>
+    
 
     <form action="update_shopinfo.php" method="post" enctype="multipart/form-data">
     <div class="profile-container">
