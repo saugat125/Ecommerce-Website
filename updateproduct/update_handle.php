2f2ffd54ec4f613ef69c72f24ebcc30a0a1d924a
Saugat-Rajbhandari <107553702+Saugat-Rajbhandari@users.noreply.github.com>
2024-05-19 13:15:48 +0545

    image issue in trader dashboard solved

--- updateproduct/update_handle.php
+++ updateproduct/update_handle.php
@@ -10,25 +10,32 @@ if (isset($_POST['submit'])) {
     $allergy = $_POST['allergy'];
     $max_order = $_POST['max_order'];
     $image = $_FILES['image']['name'];
-    $image_temp = $_FILES['image']['tmp_name']; 
+    $image_temp = $_FILES['image']['tmp_name'];
     if (empty($image)) {
         $image = $_POST['image-alt'];
-    }
-
-    $destination = "../image/" . $image; 
-    if (empty($image) || move_uploaded_file($image_temp, $destination)) {
-        $query = "UPDATE PRODUCT SET PRODUCT_NAME='$name',DESCRIPTION='$description',PRICE='$price',STOCK_AVAILABLE='$stock',MAX_ORDER='$max_order',ALLERGY_INFORMATION='$allergy',PRODUCT_IMAGE='$image' WHERE PRODUCT_ID='$product_id'";
-
+        // No new image selected, use the existing image
+        $query = "UPDATE PRODUCT SET PRODUCT_NAME='$name',DESCRIPTION='$description',PRICE='$price',STOCK_AVAILABLE='$stock',MAX_ORDER='$max_order',ALLERGY_INFORMATION='$allergy' WHERE PRODUCT_ID='$product_id'";
         $update_stmt = oci_parse($conn, $query);
-
         if (oci_execute($update_stmt)) {
             header('location: ../product-table/manage_product.php');
         } else {
             echo '<script>alert("ERROR: ' . oci_error($update_stmt) . '")</script>';
         }
     } else {
-        // Failed to move file
-        echo '<script>alert("Failed to move uploaded file.")</script>';
+
+        $destination = "../image/" . $image;
+        if (move_uploaded_file($image_temp, $destination)) {
+            $query = "UPDATE PRODUCT SET PRODUCT_NAME='$name',DESCRIPTION='$description',PRICE='$price',STOCK_AVAILABLE='$stock',MAX_ORDER='$max_order',ALLERGY_INFORMATION='$allergy',PRODUCT_IMAGE='$image' WHERE PRODUCT_ID='$product_id'";
+            $update_stmt = oci_parse($conn, $query);
+            if (oci_execute($update_stmt)) {
+                header('location: ../product-table/manage_product.php');
+            } else {
+                echo '<script>alert("ERROR: ' . oci_error($update_stmt) . '")</script>';
+            }
+        } else {
+            // Failed to move file
+            echo '<script>alert("Failed to move uploaded file.")</script>';
+        }
     }
 }
 oci_close($conn);
